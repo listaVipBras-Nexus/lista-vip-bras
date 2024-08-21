87ff0fea5fa1ec2296e6dd9862a355f5cb97e5dc
listaVipBras-Nexus <listavipbras.nexus@gmail.com>
2024-08-21 14:07:27 -0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'swiper/css/scrollbar';
+import HomePage from './pages/HomePage';
+
+export default function App() {
+    return (
+        <HomePage />
+    );
+}
